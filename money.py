89b3ac7c88b2232f3bfd7d38e8d933ac89ae2655
Zachary Davis <96398054+ZachADavis@users.noreply.github.com>
2021-12-21 18:09:27 -0500

    Update money.py

--- money.py
+++ money.py
@@ -1,19 +1,17 @@
 def money(hours, ot_hours):
-	normal_hourly = int(input("How much do you make an hour? "))
-	overtime_hourly = normal_hourly + normal_hourly/2
-	normal_hours = hours
-	overtime_hours = ot_hours
-	wr_benefits = 200
+	hourly = int(input("How much do you make an hour? "))
+	ot_hourly = hourly + (hourly/2)
+	benefits = int(input("How much do you pay in benefits? "))
 	taxes = .1839
 	fica_tax = .0765
-	normal_pay = normal_hours * normal_hourly
-	normal_taxed = (normal_pay * taxes)
-	normal_fica = (normal_pay * fica_tax)
-	total_tax = normal_taxed + normal_fica
-	normal_total = (normal_pay - total_tax) - wr_benefits
-	normal_total_clean = float(round(normal_total, 3))
-	overtime_pay = overtime_hours * overtime_hourly
+	pay = hours * normal_hourly
+	taxed = (pay * taxes)
+	fica = (pay * fica_tax)
+	total_tax = taxed + fica
+	total = (pay - total_tax) - benefits
+	total_clean = float(round(total, 3))
+	overtime_pay = ot_hours * ot_hourly
 	overtime_total = overtime_pay
-	total_pay = normal_total + overtime_total
+	total_pay = total + overtime_total
 	return normal_total_clean, overtime_total
 print(money(0, 0))
